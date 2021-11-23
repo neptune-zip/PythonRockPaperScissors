79bd08ecfb2484260b8a12f25a941f38adc75909
Kyle Neil <kyleneil1403@gmail.com>
2021-11-23 19:29:28 +0000

    Update test_RockPaperScissors.py
    linting

--- test/test_RockPaperScissors.py
+++ test/test_RockPaperScissors.py
@@ -1,13 +1,14 @@
 import unittest
 from src.Engine.RockPaperScissors import RockPaperScissors
 
+
 class RockPaperScissorTest(unittest.TestCase):
 
     rock = RockPaperScissors()
 
     def test_determine_winner(self):
-        self.assertEqual("Draw", self.rock.determine_winner(0,0))
-
+        self.assertEqual("Draw", self.rock.determine_winner(0, 0))
 
     def test_get_user_choice_request(self):
-        self.assertEqual("Select 0 for Rock 1 for Paper 2 for Scissors ", self.rock.get_user_choice_request(["Rock","Paper","Scissors"]))
+        self.assertEqual("Select 0 for Rock 1 for Paper 2 for Scissors ",
+                         self.rock.get_user_choice_request(["Rock", "Paper", "Scissors"]))
