a78369a9886c0f1613d0b409551de42bbb943d79
Kyle Neil <kyleneil1403@gmail.com>
2021-12-08 15:06:58 +0000

    combine 2 lines into 1

--- test/test_RockPaperScissors.py
+++ test/test_RockPaperScissors.py
@@ -10,5 +10,4 @@ class RockPaperScissorTest(unittest.TestCase):
         self.assertEqual("Draw", self.rock.determine_winner(0, 0))
 
     def test_get_user_choice_request(self):
-        self.assertEqual("Select 0 for Rock 1 for Paper 2 for Scissors ",
-                         self.rock.get_user_choice_request(["Rock", "Paper", "Scissors"]))
+        self.assertEqual("Select 0 for Rock 1 for Paper 2 for Scissors ", self.rock.get_user_choice_request(["Rock", "Paper", "Scissors"]))
